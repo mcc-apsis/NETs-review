fa32d9957393bcc7def387d24c1dcc9a9e0c5e10
Max <max.w.callaghan@gmail.com>
2017-07-06 13:14:14 +0200

    scraped results

--- scrape_bib.py
+++ scrape_bib.py
@@ -1,20 +1,30 @@
-import re, sys
+import re, sys, os
 import django
 
 # import file for easy access to browser database
 sys.path.append('/home/galm/software/tmv/BasicBrowser/')
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BasicBrowser.settings")
+django.setup()
+
 from scoping.models import *
 
-fpath = "/home/max/Documents/mcc/papers/NETS/NETs-review/Bibliography for GGR 2014-16 (v2) 20 Oct 2016s.html"
+fpath = "Bibliography for GGR 2014-16 (v2) 20 Oct 2016s.html"
 
 section = None
 
 tagpat = re.compile(r'<.*?>')
 refpat = "(.*)\(([2|1]{1}[0-9]{3})\) *(?:<i>)*(.*)"
 
+alldocs = Doc.objects.filter(
+    technology__isnull=False
+) | Doc.objects.filter(query__technology__isnull=False)
+
+
+
 with open(fpath,"r") as f:
-    with open("results.txt","w") as w:
+    with open("results.tsv","w") as w:
+        w.write("{}\t{}\t{}\t{}\t{}\t{}\n".format("au","py","ti","section","db","full-text"))
         for line in f:
             lines = re.split('<br/>',line)
             for l in lines:
@@ -58,15 +68,25 @@ with open(fpath,"r") as f:
                     au = tagpat.sub("",ref.group(1))
                     py = tagpat.sub("",ref.group(2))
                     ti = tagpat.sub("",ref.group(3).split("<")[0])
-                    docs = Doc.objects.filter(
-                        docauthinst__AU__icontains=au.split()[0],
-                        PY=py,
-                        title__icontains=ti.split()[0]
-                    )
-                    if docs.count()==1:
-                        doc = "http://apsis.mcc-berlin.net/scoping/doc/"+docs.first().UT
+                    try:
+                        docs = alldocs.filter(
+                            docauthinst__AU__icontains=au.split()[0],
+                            PY=py,
+                            title__icontains=ti.split()[0]
+                        ).filter(title__icontains=ti.split()[1]).filter(
+                            title__icontains=ti.split()[2]
+                        ).distinct()
+                    except:
+                        docs = alldocs.filter(UT="blabla")
+                    if docs.count()>0:
+                        doc = "https://apsis.mcc-berlin.net/scoping/document/"+docs.first().UT
                     else:
                         doc = "not found"
-                    w.write("{}\t{}\t{}\t{}\t{}\n".format(au,py,ti,section,doc))
+                    if docs.count() > 1:
+                        print("\n###################")
+                        print(ti)
+                        for d in docs:
+                            print(d.title)
+                    w.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(au,py,ti,section,doc,l))
                 #else:
                     #print(l)
