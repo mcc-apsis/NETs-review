600f6b1ff26eab0ae76613c406fcdf9a8f952aac
mcallaghan <max.w.callaghan@gmail.com>
2017-07-11 16:46:19 +0200

    queries

--- create_queries.py
+++ create_queries.py
@@ -10,15 +10,19 @@ with open("results.tsv") as results:
                 else:
                     ftype="w"
                     resfiles.append(fields[3])
+                ti = fields[2].split(".")[0]
+                if len(ti)==0:
+                    continue
+                au = fields[0].split(",")[0]
                 for source in ["wos","scopus"]:
                     with open("queries/{}_{}.txt".format(fields[3],source),ftype) as w:
                         if ftype =="a":
                             w.write(' OR ')
                         if source=="wos":
-                            w.write('(TI=("{}") AND AU=("{}") AND PY=("{}"))'.format(
-                                fields[2],fields[0], fields[1]
+                            w.write('(TI=("{}") AND AU=("{}") AND PY=({}))'.format(
+                                ti ,au, fields[1]
                             ))
                         else:
-                            w.write('(TITLE("{}") AND AUTH("{}") AND PUBYEAR("{}"))'.format(
-                                fields[2],fields[0], fields[1]
+                            w.write('(TITLE("{}") AND AUTH("{}") AND PUBYEAR({}))'.format(
+                                ti ,au, fields[1]
                             ))
