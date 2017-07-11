92e6989a253ac019943a4e7332e64cadf0720b38
mcallaghan <max.w.callaghan@gmail.com>
2017-07-11 11:25:29 +0200

    queries created

---
+++ create_queries.py
@@ -0,0 +1,24 @@
+resfiles = []
+with open("results.tsv") as results:
+    for line in results:
+        fields = line.split("\t")
+        if len(fields) > 1:
+            fields[3] = fields[3].replace("/","_")
+            if fields[4] == "not found":
+                if fields[3] in resfiles:
+                    ftype="a"
+                else:
+                    ftype="w"
+                    resfiles.append(fields[3])
+                for source in ["wos","scopus"]:
+                    with open("queries/{}_{}.txt".format(fields[3],source),ftype) as w:
+                        if ftype =="a":
+                            w.write(' OR ')
+                        if source=="wos":
+                            w.write('(TI=("{}") AND AU=("{}") AND PY=("{}"))'.format(
+                                fields[2],fields[0], fields[1]
+                            ))
+                        else:
+                            w.write('(TITLE("{}") AND AUTH("{}") AND PUBYEAR("{}"))'.format(
+                                fields[2],fields[0], fields[1]
+                            ))
