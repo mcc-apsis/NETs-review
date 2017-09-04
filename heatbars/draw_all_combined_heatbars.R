ec7b8dcc34825ff1926ef5bd1b396eb29f1e5de8
mcallaghan <max.w.callaghan@gmail.com>
2017-09-04 09:55:56 +0200

    icons

--- heatbars/draw_all_combined_heatbars.R
+++ heatbars/draw_all_combined_heatbars.R
@@ -10,6 +10,21 @@ library(countrycode)
 
 source("heatbars/heatbar_functions.R")
 
+############################################################
+## Get pics
+
+image.file <- dir("icons", pattern=".png", full.names=TRUE)
+
+image.file <- image.file[order(as.integer(sub("_.*","",sub("icons/","",image.file))))]
+npoints <- length(image.file)
+pics  <- vector(mode="list", length=npoints)
+for(i in 1:npoints) {
+  pics[[i]] <- EBImage::readImage(image.file[i])
+}
+names(pics) <- sub(".png","",sub("icons/","",image.file))
+
+
+
 # Authorise googlesheets to access your Google Sheets account
 gs_auth()
 
@@ -61,6 +76,15 @@ costs2050 <- countranges(
   ), 
   techs, "range")
 
+rlabs <- costs2050 %>%
+  group_by(resource) %>%
+  summarise(
+    resourcelab=paste0(gsub(" (terrestrial and ocean)","",first(resource),fixed=T),'\n[',max(maxvalue),' studies]')
+  )
+
+
+names(pics) <- rlabs$resourcelab
+
 
 heatbar(costs2050,"pcnt") +
   theme(axis.text.x = element_text(angle=60, hjust=1,vjust=1)) + 
@@ -68,11 +92,14 @@ heatbar(costs2050,"pcnt") +
 
 ggsave("plots/heatbars/all_costs.png")
 
+
+
 heatbar(costs2050,"pcnt",text=T) +
   theme(axis.text.x = element_text(angle=60, hjust=1,vjust=1)) + 
-  labs(x="Technology",y="Costs in $US(2011)/tCO2") 
+  labs(x="",y="Costs in $US(2011)/tCO2") +
+  theme(axis.text.x  = my_axis(pics))
 
-ggsave("plots/heatbars/all_costs_labelled.png")
+ggsave("plots/heatbars/all_costs_labelled.png", width=16,height=10)
 
 
 
@@ -99,15 +126,12 @@ dataf <- filter(
   )
 
 ggplot() +
-  # geom_linerange(
-  #   data=dataf,
-  #   aes(x=PYJ,ymin=min,ymax=max, colour=technology),
-  #   size=1
-  # ) +
   geom_crossbar(
     data=dataf,
-    aes(x=PYJ,ymin=min,ymax=max, colour=technology),
-    size=1
+    aes(x=PYJ, ymin=min, ymax=max,y=max, fill=technology),
+    size=0.001,
+    width=0.2,
+    alpha=0.5
   ) +
   geom_point(
     data=dataf,
@@ -115,7 +139,32 @@ ggplot() +
     size=2   
   ) +
   theme_bw() +
-  scale_color_brewer(palette="Set2")+
+  scale_color_brewer(palette="Set2") +
+  scale_fill_brewer(palette="Set2") +
   labs(x="Study Year",y="Costs in $US(2011)/tCO2")
 
 ggsave("plots/heatbars/all_costs_years.png")
+
+
+ggplot() +
+  geom_crossbar(
+    data=dataf,
+    aes(x=PYJ, ymin=min, ymax=max,y=max, fill=technology),
+    size=0.001,
+    width=0.2,
+    alpha=0.5
+  ) +
+  geom_point(
+    data=dataf,
+    aes(x=PYJ,y=estimate, colour=technology, shape=technology),
+    size=2   
+  ) +
+  theme_bw() +
+  scale_color_brewer(palette="Set2") +
+  scale_fill_brewer(palette="Set2") +
+  labs(x="Study Year",y="Costs in $US(2011)/tCO2") +
+  facet_grid(technology~.)
+
+ggsave("plots/heatbars/all_costs_years_faceted.png")
+
+
--- heatbars/heatbar_functions.R
+++ heatbars/heatbar_functions.R
@@ -204,7 +204,7 @@ heatbar <- function(df,f,step=1, fixed=T, text=F) {
       pcnt = median(pcnt)
     ) %>%
     mutate(
-      text = paste0(min,"-",max,": ",round(pcnt),"% agreement")
+      text = paste0(min,"-",max,"\n",round(pcnt),"%")
     )
     
   cscale <- calc_cscale(df, f, flab, fixed)
@@ -239,8 +239,9 @@ heatbar <- function(df,f,step=1, fixed=T, text=F) {
       geom_text(
         data=dfagreement,
         aes(resourcelab, med, label=text),
-        angle=90,
-        hjust=0,
+        #angle=90,
+        #hjust=0,
+        vjust=0,
         nudge_y=20
       )
   }
@@ -341,3 +342,43 @@ heatbar_years <- function(data, df, f, grp=NA, fixed=TRUE) {
   
   return(dataf)
 }
+
+
+
+library(ggplot2)  ## devtools::install_github("hadley/ggplot2)
+library(grid)     ## rasterGrob
+library(EBImage)  ## readImage
+library(ggthemes) ## theme_minimal
+
+## ##########
+## INDEPENDENT CODE TO BE SOURCED:
+## ##########
+# user-level interface to the element grob
+my_axis = function(img) {
+  structure(
+    list(img=img),
+    class = c("element_custom","element_blank", "element") # inheritance test workaround
+  )
+}
+# returns a gTree with two children: the text label, and a rasterGrob below
+element_grob.element_custom <- function(element, x,...)  {
+  stopifnot(length(x) == length(element$img))
+  tag <- names(element$img)
+  # add vertical padding to leave space
+  g1 <- textGrob(paste0(tag, "\n\n\n\n\n"), x=x, vjust=1.2)
+  g2 <- mapply(rasterGrob, x=x, image=element$img[tag], 
+               MoreArgs=list(vjust=0.1, interpolate=FALSE,
+                             height=unit(3,"lines")),
+               SIMPLIFY=FALSE)
+  
+  gTree(children=do.call(gList, c(list(g1), g2)), cl="custom_axis")
+}
+# gTrees don't know their size and ggplot would squash it, so give it room
+grobHeight.custom_axis = heightDetails.custom_axis = function(x, ...)
+  unit(6, "lines")
+## ##########
+## END
+## ##########
+
+
+
