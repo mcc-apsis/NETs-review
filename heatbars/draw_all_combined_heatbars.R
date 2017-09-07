b48cf492d42809f0f02a965a7e53267d69e50f44
mcallaghan <max.w.callaghan@gmail.com>
2017-09-07 13:27:00 +0200

    rects script

--- heatbars/draw_all_combined_heatbars.R
+++ heatbars/draw_all_combined_heatbars.R
@@ -564,11 +564,17 @@ ggplot(bystudy) +
 ## By technology ranges
 
 bytech <- all_data %>%
-  filter(!is.na(value) , is.finite(value)) %>%
+  filter(
+    !is.na(value) , 
+    is.finite(value),
+    costsinclude==T, potsinclude==T
+    ) %>%
   group_by(technology, variable) %>% 
   summarise(
-    min = min(value, na.rm = T),
-    max = max(value, na.rm = T)
+    # min = min(value, na.rm = T),
+    # max = max(value, na.rm = T)
+    min = quantile(value, .25, na.rm = T),
+    max = quantile(value, .75, na.rm = T)
   )
 
 costsums <- filter(bytech, variable=="cost") %>%
@@ -581,13 +587,25 @@ all_sums <- left_join(costsums, potsums)
 
 ggplot() + 
   geom_rect(
-    aes(
+    data=all_sums,
+    mapping=aes(
       xmin=pots_min,
       xmax=pots_max,
       ymin=costs_min,
-      ymax=costs_max
-      )
-  )
+      ymax=costs_max,
+      color=technology
+      ),
+    alpha=0.0,
+    size=2
+   # color="black"
+  ) + theme_bw() +
+  labs(x="Potentials in Gt CO2/year",y="Costs in $/tCO2")
+
+ggsave("plots/synthetic/rects.svg",width=16,height=10)
+
++
+  scale_y_log10() +
+  scale_x_log10()
 
 
 
