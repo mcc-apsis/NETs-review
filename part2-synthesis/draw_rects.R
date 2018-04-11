1cc210a251755ba8b1d573e658b3bf5172874315
Max Callaghan <galm@mcc-berlin.net>
2018-04-11 17:26:06 +0200

    more quantiles

--- part2-synthesis/draw_rects.R
+++ part2-synthesis/draw_rects.R
@@ -7,7 +7,7 @@ library(ggplot2)
 load("data/all_data.RData")
 
 
-
+all_data[all_data$technology=="Ocean alkalinisation",]$technology = "Enhanced weathering (terrestrial and ocean)"
 
 #############
 ## Costs
@@ -22,6 +22,8 @@ costs <- all_data %>%
   summarise(
     costs_q25 = quantile(value, .25, na.rm = T),
     costs_q75 = quantile(value, .75, na.rm = T),
+    costs_q10 = quantile(value, .10, na.rm = T),
+    costs_q90 = quantile(value, .90, na.rm = T),
     costs_min = min(value, na.rm = T),
     costs_max = max(value, na.rm = T)
   )
@@ -54,6 +56,8 @@ pots <- all_data %>%
   summarise(
     pots_q25 = quantile(value, .25, na.rm = T),
     pots_q75 = quantile(value, .75, na.rm = T),
+    pots_q10 = quantile(value, .10, na.rm = T),
+    pots_q90 = quantile(value, .90, na.rm = T),
     pots_min = min(value, na.rm = T),
     pots_max = max(value, na.rm = T)
   )
@@ -84,15 +88,17 @@ all_sums$pots_max[is.na(all_sums$pots_max)] <- 100
 
 expert_judgements <- data.frame(
   technology = unique(all_sums$technology),
-  pots_min = c(0.5, 0.5, 0.3, 0.5,2,NA,NA,3),
-  pots_max = c(3.6, 5, 2, 5, 4, NA, NA, 6),
-  costs_min = c(5, 100, 0, 100, 50, NA, NA, 0),
-  costs_max = c(50, 200, 120, 300, 200, NA, NA, 100)
+  pots_min_expert = c(0.5, 0.5, 0.3, 0.5,2,NA,3),
+  pots_max_expert = c(3.6, 5, 2, 5, 4,  NA, 6),
+  costs_min_expert = c(5, 100, 0, 100, 50,  NA, 0),
+  costs_max_expert = c(50, 200, 120, 300, 200, NA, 100)
 )
 
 write.csv(expert_judgements,"tables/expert_judgements.csv")
 
+pots <- left_join(pots,select(expert_judgements,technology,pots_min_expert, pots_max_expert))
 
+costs <- left_join(costs,select(expert_judgements,technology,costs_min_expert, costs_max_expert))
 
 ggplot() + 
   geom_rect(
@@ -165,10 +171,10 @@ ggsave("plots/synthetic/rects_doctored.svg",width=16,height=10)
 ggplot() + geom_rect(
   data=expert_judgements,
   mapping=aes(
-    xmin=pots_min,
-    xmax=pots_max,
-    ymin=costs_min,
-    ymax=costs_max,
+    xmin=pots_min_expert,
+    xmax=pots_max_expert,
+    ymin=costs_min_expert,
+    ymax=costs_max_expert,
     colour=technology
   ),
   alpha=0.0,
